8aab32e6bfcfcdc52384af7f2560f4ffeb080314
Anhdt <dtavnn@gmail.com>
2024-07-30 22:10:19 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
 #2. Clear the login password
-#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme openwrt没有
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
