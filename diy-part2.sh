bdaf2cd6614086c96bb19764f577cbaae532a539
Anhdt <dtavnn@gmail.com>
2024-07-31 07:04:28 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/9.0.0.12/g' package/base-files/files/bin/config_generate
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #2. timezone
-sed -i 's/CST-8/<+07>-7/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/CST-8/<+07>-7/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/Shanghai/Bangkok/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme openwrt没有
