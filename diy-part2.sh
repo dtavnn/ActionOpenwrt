42cb3cebe66e3ff6cc609870e8a76b4267f07c01
Anhdt <dtavnn@gmail.com>
2024-07-31 07:17:45 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,12 @@
 sed -i 's/192.168.1.1/9.0.0.12/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 #2. timezone
-#sed -i 's/CST-8/<+07>-7/g' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/Shanghai/Bangkok/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/CST-8/<+07>-7/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/Shanghai/Bangkok/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/zh_cn/vi_vn/g' package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme openwrt没有
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
